835aeb83abc534eb88bc957a5c6ec8ab891e9429
Tanvir Chowdhury <tanvirchowdhury698@gmail.com>
2023-12-20 22:28:53 +0600

    Ajax call added in Registration-form & Send-OTP-form

--- resources/views/components/auth/registration-form.blade.php
+++ resources/views/components/auth/registration-form.blade.php
@@ -39,3 +39,49 @@
         </div>
     </div>
 </div>
+
+<script>
+    async function onRegistration(){
+        let email = document.getElementById('email').value;
+        let firstName = document.getElementById('firstName').value;
+        let lastName = document.getElementById('lastName').value;
+        let mobile = document.getElementById('mobile').value;
+        let password = document.getElementById('password').value;
+
+        if(email.length===0){
+            errotToast("Please enter your email address")
+        }
+        else if(firstName.length===0){
+            errotToast("Please enter your First Name")
+        }
+        else if(lastName.length===0){
+            errotToast("Please enter your Last Name")
+        }
+        else if(mobile.length===0){
+            errotToast("Please enter your Mobile")
+        }
+        else if(password.length===0){
+            errotToast("Please enter your Password")
+        }
+        else{
+            showLoader();
+            let res=await axios.post("/user-registration",{
+                email:email,
+                firstName:firstName,
+                lastName:lastName,
+                mobile:mobile,
+                password:password   
+            });
+            hideLoader();
+            if(res.status===200 && res.data['status']==='success'){
+                successToast(res.data['message']);
+                setTimeout(function (){
+                    window.location.href="/userLogin"
+                }, 2000);
+            }
+            else{
+                errorToast(res.data['message']);
+            }
+        }
+    }
+</script>
--- resources/views/components/auth/send-otp-form.blade.php
+++ resources/views/components/auth/send-otp-form.blade.php
@@ -14,4 +14,28 @@
         </div>
     </div>
 </div>
-
+<script>
+    async function VerifyEmail(){
+        let email = document.getElementById('email').value;
+        if(email.length===0){
+            errotToast("Please enter your email address")
+        }
+        else{
+            showLoader();
+            let res=await axios.post("/send-otp",{
+                email:email
+            });
+            hideLoader();
+            if(res.status===200 && res.data['status']==='success'){
+                successToast(res.data['message']);
+                sessionStorage.setItem('email',email);
+                setTimeout(function (){
+                    window.location.href="/verifyOtp"
+                }, 1000);
+            }
+            else{
+                errorToast(res.data['message']);
+            }
+        }
+    }
+</script>
